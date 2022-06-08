b52b0a42d4153cf4525f68ef68e6c1d5cfa7cde3
elifyyalcin <elifyyyalcin@gmail.com>
2022-06-09 00:40:37 +0300

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Elif Yalçın on 9.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
